e64e37fe6dd3b8d0977c1b4df13d10984f462388
Ciprian Cornea <cipri.cornea@outlook.com>
2021-12-03 20:09:19 +0200

    update

--- src/app/modules/about/about.module.ts
+++ src/app/modules/about/about.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AboutPageComponent
   ],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        RouterModule
+    ]
 })
 export class AboutModule { }
